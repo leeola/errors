7bc82c51111882d4cf6d638bbdb894467a8cc7b8
Lee Olayvar <leeolayvar@gmail.com>
2017-05-30 08:19:43 -0700

    feat: added basic multi error support

---
+++ multierrors.go
@@ -0,0 +1,49 @@
+package errors
+
+import "bytes"
+
+const (
+	defaultErrorsSep = "; "
+)
+
+func Join(errs []error) error {
+	return multiErr{
+		Errs: errs,
+		Sep:  defaultErrorsSep,
+	}
+}
+
+func JoinSep(errs []error, sep string) error {
+	return multiErr{
+		Errs: errs,
+		Sep:  sep,
+	}
+}
+
+func Split(err error) []error {
+	sErr, ok := err.(multiErr)
+	if !ok {
+		return []error{err}
+	}
+
+	return sErr.Errs
+}
+
+type multiErr struct {
+	Errs []error
+	Sep  string
+}
+
+func (e multiErr) Error() string {
+	var buf bytes.Buffer
+
+	for i, err := range e.Errs {
+		if i != 0 {
+			buf.WriteString(e.Sep)
+		}
+
+		buf.WriteString(err.Error())
+	}
+
+	return buf.String()
+}
